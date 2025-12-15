7e7769729fbc3a910295b8452eb90a2a5abd5370
wuubzi <carlosasalas321@gmail.com>
2025-12-15 02:24:10 -0500

    fixing problems with cors in production dashboard

--- src/main/java/com/healthUnity/backend/Config/SecurityConfig.java
+++ src/main/java/com/healthUnity/backend/Config/SecurityConfig.java
@@ -44,7 +44,8 @@ public class SecurityConfig {
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
         configuration.setAllowedOrigins(List.of(
-                "http://localhost:4200"
+                "http://localhost:4200",
+                "https://healthunity-dashboard.vercel.app/"
         ));
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
